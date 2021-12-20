487dd50332297a3cc782afe4ff704b21f8d91e55
aryanjain21 <aryanjain1997@gmail.com>
2021-12-20 23:06:22 +0530

    Added search functionality & updated side nav

--- src/common/search-bar/search-bar.jsx
+++ src/common/search-bar/search-bar.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './search-bar.scss';
+import ColorList from '../../assets/locales/color.json';
 import Search from '../../assets/icons/search.svg';
+import { useNote } from '../../context/noteContext';
+import { getNotes } from '../../services/api';
+import BackArrow from '../../assets/icons/back_arrow.svg';
 
 const SearchBar = (props) => {
 
+    const { noteDispatch } = useNote();
     const { mobileSearch, setMobileSearch } = props;
+    const [search, setSearch] = useState('');
+
+    const searchNote = async (value) => {
+        try {
+            const res = await getNotes({search: value});
+            if(res.data.status == '200') {
+                res.data.data.map(note => {
+                    note.color = note.color ? ColorList.find(color => color.key === note.color).id : 1
+                });
+                noteDispatch({type: 'ALL_NOTE', payload: res.data})
+            }
+        } catch (error) {
+            
+        }
+    }
 
     return (
         <div className={`search_area ${mobileSearch ? 'mobile_search_area' : ''}`}>
@@ -15,12 +35,14 @@ const SearchBar = (props) => {
                     </div>
                     :
                     <div className='icon_area' onClick={() => setMobileSearch(!mobileSearch)}>
-                        close
+                        <img src={BackArrow} />
                     </div>
                 }
                 <input className='form_input'
                     placeholder='Search'
-                    type="text" />
+                    type="text"
+                    value={search}
+                    onChange={(e) => {setSearch(e.target.value); searchNote(e.target.value);}} />
             </div>
         </div>
     );
--- src/common/side-nav/side-nav.jsx
+++ src/common/side-nav/side-nav.jsx
@@ -1,29 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './side-nav.scss';
+import ColorList from '../../assets/locales/color.json';
 import Notes from '../../assets/icons/notes.svg';
 import Archive from '../../assets/icons/archive.svg';
 import Trash from '../../assets/icons/trash.svg';
+import { getNotes } from '../../services/api';
+import { useUser } from '../../context/userContext';
+import { useNote } from '../../context/noteContext';
 
 const SideNav = (props) => {
 
+    const { user, userDispatch } = useUser();
+    const { noteDispatch } = useNote();
     const { showSideNav } = props;
+
+    const handleClick = (screen) => {
+        userDispatch({ type: 'SCREEN_PREFERENCE', payload: { screen: screen } })
+        if (screen === "Notes") {
+            getAllNotes({ isActive: 1, isArchived: 0 })
+        } else if (screen === "Archive") {
+            getAllNotes({ isActive: 1, isArchived: 1 })
+        } else {
+            getAllNotes({ isActive: 0 })
+        }
+        localStorage.setItem('setUser', JSON.stringify({ ...user, screen: screen }))
+    }
+
+    const getAllNotes = async (obj) => {
+        try {
+            let presentScreen = {
+                section: {
+                    isActive: obj && obj.isActive ? obj.isActive : 0,
+                    isArchived: obj && obj.isArchived ? obj.isArchived : 0
+                }
+            }
+            let res = await getNotes(presentScreen);
+            res.data.data.map(note => {
+                note.color = note.color ? ColorList.find(color => color.key === note.color).id : 1
+            });
+            noteDispatch({ type: 'ALL_NOTE', payload: res.data });
+        } catch (error) {
+
+        }
+    }
 
     return (
         <div className='side_nav_container'>
             <ul className={`nav_list ${showSideNav ? 'show_list' : ''}`}>
-                <li className='list notes'>
+                <li className={`list notes ${user.screen === 'Notes' ? 'active_nav' : ''} ${showSideNav && user.screen === 'Notes' && 'open_view'}`} onClick={() => { handleClick("Notes") }}>
                     <div className='icon'>
                         <img src={Notes} />
                     </div>
                     {showSideNav && <span className='txt'>Note</span>}
                 </li>
-                <li className='list'>
+                <li className={`list ${user.screen === 'Archive' ? 'active_nav' : ''} ${showSideNav && user.screen === 'Archive' && 'open_view'}`} onClick={() => { handleClick("Archive") }}>
                     <div className='icon'>
                         <img src={Archive} />
                     </div>
                     {showSideNav && <span className='txt'>Archive</span>}
                 </li>
-                <li className='list'>
+                <li className={`list ${user.screen === 'Trash' ? 'active_nav' : ''} ${showSideNav && user.screen === 'Trash' && 'open_view'}`} onClick={() => { handleClick("Trash") }}>
                     <div className='icon'>
                         <img src={Trash} />
                     </div>
